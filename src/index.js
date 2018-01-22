0d37898a3667ac444099cd5b5e9dcd8f87cadc62
Matt <matt.smith@digital.gov.au>
2018-01-23 10:58:20 +1100

    add graphql route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 var express = require('express');  
 var app = express();  
 var db  = require('./db');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var port = 4000;
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port);  
 console.log("Listening on port", port);  

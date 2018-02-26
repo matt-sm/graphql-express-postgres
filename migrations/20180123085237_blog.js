28ac030d3e437d0d8c889a65b9c404e2208043f4
Matt <matt.smith@digital.gov.au>
2018-02-27 10:33:03 +1100

    prettier the migrations

--- migrations/20180123085237_blog.js
+++ migrations/20180123085237_blog.js
@@ -1,44 +1,45 @@
-
 exports.up = async (knex, Promise) => {
-return await Promise.all([
+  return await Promise.all([
+    knex.schema.createTable('user', table => {
+      table.increments('id').primary()
+      table.string('username')
+      table.string('password')
+      table.string('name')
+      table.string('email')
+      table.timestamps()
+    }),
 
-        knex.schema.createTable('user', function(table) {
-            table.increments('id').primary();
-            table.string('username');
-            table.string('password');
-            table.string('name');
-            table.string('email');
-            table.timestamps();
-        }),
+    knex.schema.createTable('post', table => {
+      table.increments('id').primary()
+      table.string('title')
+      table.string('body')
+      table
+        .integer('author_id')
+        .references('id')
+        .inTable('user')
+      table.dateTime('postDate')
+    }),
 
-        knex.schema.createTable('post', function(table){
-            table.increments('id').primary();
-            table.string('title');
-            table.string('body');
-            table.integer('author_id')
-                 .references('id')
-                 .inTable('user');
-            table.dateTime('postDate');
-        }),
-
-        knex.schema.createTable('comment', function(table){
-            table.increments('id').primary();
-            table.string('body');
-            table.integer('author_id')
-                 .references('id')
-                 .inTable('user');
-            table.integer('post_id')
-                 .references('id')
-                 .inTable('post');
-            table.dateTime('postDate');
-        })
-    ])  
-};
+    knex.schema.createTable('comment', table => {
+      table.increments('id').primary()
+      table.string('body')
+      table
+        .integer('author_id')
+        .references('id')
+        .inTable('user')
+      table
+        .integer('post_id')
+        .references('id')
+        .inTable('post')
+      table.dateTime('postDate')
+    })
+  ])
+}
 
 exports.down = async (knex, Promise) => {
-    return await Promise.all([
-            knex.schema.dropTable('user'),
-            knex.schema.dropTable('post'),
-            knex.schema.dropTable('comment')
-        ])  
-};
+  return await Promise.all([
+    knex.schema.dropTable('user'),
+    knex.schema.dropTable('post'),
+    knex.schema.dropTable('comment')
+  ])
+}
--- migrations/20180226134020_remove_username.js
+++ migrations/20180226134020_remove_username.js
@@ -1,12 +1,11 @@
-
 exports.up = async (knex, Promise) => {
   return await knex.schema.table('user', table => {
     table.dropColumn('username')
   })
-};
+}
 
 exports.down = async (knex, Promise) => {
   return await knex.schema.table('user', table => {
     table.string('username')
   })
-};
+}

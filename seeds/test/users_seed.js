4514b239b934f33ba3019a4cd33c6b6843c78b6f
Matt <matt.smith@digital.gov.au>
2018-03-09 16:42:03 +1100

    truncate all tables

--- seeds/test/users_seed.js
+++ seeds/test/users_seed.js
@@ -1,6 +1,5 @@
 exports.seed = async (knex, Promise) => {
-  await knex('user').del()
-  await knex.schema.raw('ALTER SEQUENCE user_id_seq RESTART;')
+  await knex.schema.raw('truncate table "user" restart identity cascade;')
 
   return knex('user').insert([
     {email: 'user@test.com', password: '$2a$10$WEG6o/BJE6k.TAPc.g8HJue39EcXxnwahHnxWVqaKk2.Fjx06xdWu'}

a9405f38a165883fdf3041a4dc2962b8222468b6
Matt <matt.smith@digital.gov.au>
2018-03-16 17:22:19 +1100

    comments test

--- seeds/test/users_seed.js
+++ seeds/test/users_seed.js
@@ -4,7 +4,10 @@ exports.seed = async (knex, Promise) => {
   await knex('user').insert([
     {email: 'user@test.com', password: '$2a$10$WEG6o/BJE6k.TAPc.g8HJue39EcXxnwahHnxWVqaKk2.Fjx06xdWu'}
   ]);
-  return await knex('post').insert([
+  await knex('post').insert([
     {title: 'post title', body: 'post body', author_id: 1}
+  ]);
+  return await knex('comment').insert([
+    {body: 'comment body', post_id: 1, author_id: 1}
   ]);
 };
--- src/schema.test.js
+++ src/schema.test.js
@@ -15,7 +15,12 @@ test('post.user returns single user', async () => {
   expect(await resolvers.Post.user({ author_id: 1 })).toHaveProperty('email', 'user@test.com')
 })
 
+test('post.comments returns comments for post', async () => {
+  const expected = [{ author_id: 1, body: 'comment body', id: 1, postDate: null, post_id: 1 }]
+  expect(await resolvers.Post.comments({ id: 1 })).toEqual(expected)
+})
+
 test('user.posts returns posts', async () => {
-  const expected = [{author_id: 1, body: 'post body', id: 1, postDate: null, title: 'post title'}]
+  const expected = [{ author_id: 1, body: 'post body', id: 1, postDate: null, title: 'post title' }]
   expect(await resolvers.User.posts({ id: 1 }, {})).toEqual(expect.arrayContaining(expected))
 })

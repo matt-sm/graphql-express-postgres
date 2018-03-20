f5e5dc37fc1b2a064a2d00f3a0d44f44b081d714
Matt <matt.smith@digital.gov.au>
2018-03-20 20:29:26 +1100

    remove promises from migrations

--- migrations/20180123085237_blog.js
+++ migrations/20180123085237_blog.js
@@ -1,45 +1,41 @@
-export const up = async (knex, Promise) => {
-  return await Promise.all([
-    knex.schema.createTable('user', table => {
-      table.increments('id').primary()
-      table.string('username')
-      table.string('password')
-      table.string('name')
-      table.string('email')
-      table.timestamps()
-    }),
+export const up = async knex => {
+  await knex.schema.createTable('user', table => {
+    table.increments('id').primary()
+    table.string('username')
+    table.string('password')
+    table.string('name')
+    table.string('email')
+    table.timestamps()
+  })
 
-    knex.schema.createTable('post', table => {
-      table.increments('id').primary()
-      table.string('title')
-      table.string('body')
-      table
-        .integer('author_id')
-        .references('id')
-        .inTable('user')
-      table.dateTime('postDate')
-    }),
+  await knex.schema.createTable('post', table => {
+    table.increments('id').primary()
+    table.string('title')
+    table.string('body')
+    table
+      .integer('author_id')
+      .references('id')
+      .inTable('user')
+    table.dateTime('postDate')
+  })
 
-    knex.schema.createTable('comment', table => {
-      table.increments('id').primary()
-      table.string('body')
-      table
-        .integer('author_id')
-        .references('id')
-        .inTable('user')
-      table
-        .integer('post_id')
-        .references('id')
-        .inTable('post')
-      table.dateTime('postDate')
-    })
-  ])
+  return await knex.schema.createTable('comment', table => {
+    table.increments('id').primary()
+    table.string('body')
+    table
+      .integer('author_id')
+      .references('id')
+      .inTable('user')
+    table
+      .integer('post_id')
+      .references('id')
+      .inTable('post')
+    table.dateTime('postDate')
+  })
 }
 
-export const down = async (knex, Promise) => {
-  return await Promise.all([
-    knex.schema.dropTable('user'),
-    knex.schema.dropTable('post'),
-    knex.schema.dropTable('comment')
-  ])
+export const down = async knex => {
+  await knex.schema.dropTable('user')
+  await knex.schema.dropTable('post')
+  return await knex.schema.dropTable('comment')
 }
--- migrations/20180226134020_remove_username.js
+++ migrations/20180226134020_remove_username.js
@@ -1,10 +1,10 @@
-export const up = async (knex, Promise) => {
+export const up = async knex => {
   return await knex.schema.table('user', table => {
     table.dropColumn('username')
   })
 }
 
-export const down = async (knex, Promise) => {
+export const down = async knex => {
   return await knex.schema.table('user', table => {
     table.string('username')
   })

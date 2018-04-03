71c332775c23fb075716a47b6e12802d3887f88c
Matt <matt.smith@digital.gov.au>
2018-04-03 16:29:01 +1000

    test user.posts by post id

--- src/schema.test.js
+++ src/schema.test.js
@@ -35,6 +35,7 @@ test('post.comments returns comments for post', async () => {
 test('user.posts returns posts', async () => {
   const expected = [{ author_id: 1, body: 'post body', id: 1, postDate: null, title: 'post title' }]
   expect(await resolvers.User.posts({ id: 1 }, {})).toEqual(expected)
+  expect(await resolvers.User.posts({ id: 1 }, { id: 1 })).toEqual(expected)
 })
 
 test('createToken does not throw when valid credentials', async () => {
